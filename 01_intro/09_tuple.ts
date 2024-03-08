5f2aa47663f040da6ba4f59e008a820c717f0022
Sovan Roy <roysovan00@gmail.com>
2024-03-08 17:51:40 +0530

    tuples added

---
+++ 01_intro/09_tuple.ts
@@ -0,0 +1,12 @@
+// const user: (string | number)[] = ["sr", 1];
+
+let user: [string, number, boolean];
+
+user = ["sovan", 10, true]; // not the elements but the specific order is also matters
+
+let rgb: [number, number, number] = [255, 123, 121];
+
+type User = [number, string];
+
+const newUser: User = [112, "example@gmail.com"];
+
