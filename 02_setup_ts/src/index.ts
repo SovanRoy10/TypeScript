8e7bf6750ab43d0cd1e30feb2531ce3bd169d1c3
Sovan Roy <roysovan00@gmail.com>
2024-03-08 19:49:06 +0530

    added private and public methods

--- 02_setup_ts/src/index.ts
+++ 02_setup_ts/src/index.ts
@@ -1,4 +1,4 @@
-class User {
+/*class User {
   email: string;
   name: string;
   readonly city: string = "kolkata";
@@ -9,4 +9,34 @@ class User {
 
 const sovan = new User("roysovan@gmail.com", "sovan");
 //  sovan.city = "midnapur"; // Cannot assign to 'city' because it is a read-only property.
-console.log(sovan);
+console.log(sovan); */
+
+
+/*
+//private , public
+class User {
+  private email: string;
+  name: string;
+  private readonly city: string = "kolkata";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const sovan = new User("roysovan@gmail.com", "sovan");
+//   console.log(sovan.city);  // Property 'city' is private and only accessible within class 'User'
+
+class User1 {
+  constructor(
+    public email: string, 
+    public name: string,
+    private userId : string
+    ) {
+
+    }
+} */
+
+
+
+// getters and setters 
